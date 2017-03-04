90bd33c72668967fd1ab718c64b2cf2ee06d8c42
Andy Gildehaus <agildehaus@gmail.com>
2017-03-04 16:30:39 -0600

    Give floating action menu a translucent background.

--- app/src/main/java/co/blustor/passwordvault/activities/GroupActivity.java
+++ app/src/main/java/co/blustor/passwordvault/activities/GroupActivity.java
@@ -1,7 +1,5 @@
 package co.blustor.passwordvault.activities;
 
-import android.app.Activity;
-import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityOptionsCompat;
